123cb14d1e4fee332d54f2d83d757e9af26e6142
Matthew Falbo <matthew.falbo@gmail.com>
2014-05-06 16:18:45 -0600

    Functional exercise.  Reads csv file, converts data to an array of objects, stringifies with JSON and writes to output.js.

--- csv-to-json.js
+++ csv-to-json.js
@@ -1,4 +1,27 @@
 var fs = require('fs');
+
 var csv = fs.readFileSync(process.argv[2],'utf-8');
-var x = csv.split(',');
-console.log(x);
+
+
+var csvArray = csv.split('\n');
+var newArray = [];
+
+for(var i=0; i<csvArray.length; i++){
+	var item = csvArray[i].split(',');
+	newArray.push(item);
+}
+
+var objArray = [];
+
+for(var i=1; i<newArray.length; i++){
+	var obj = {};
+	obj[newArray[0][0]] = newArray[i][0];
+	obj[newArray[0][1]] = newArray[i][1];
+	obj[newArray[0][2]] = newArray[i][2];
+
+	objArray.push(obj);
+}
+
+objString = JSON.stringify(objArray);
+
+fs.writeFileSync(process.argv[3], objString);
